9060c85705d231c51d005e1e006f12f98c2554f1
Reena <rkreena07@test.com>
2018-12-20 14:38:19 +0530

    ecrt changes

--- routes/ecbt.js
+++ routes/ecbt.js
@@ -11,6 +11,7 @@ var gateway = braintree.connect({
 app.get("/client_token", function (req, res) {
   console.log('client_token');
   gateway.clientToken.generate({}, function (err, response) {
+    console.log(response.clientToken);
     res.send(response.clientToken);
   });
 });
@@ -66,9 +67,6 @@ app.get("/order/:id", function(req, res){
 app.post("/checkout", function (req, res) {
   console.log('checkout');
     var nonce = req.body.nonce;
-    console.log(req.body.amount);
-    console.log(nonce);
-    console.log(req.body.currency);
     var saleRequest = {
         amount: req.body.amount,
         merchantAccountId: req.body.currency?req.body.currency:"INR",
@@ -90,7 +88,7 @@ app.post("/checkout", function (req, res) {
           res.send(err.message);
         } else if (result.success) {
           console.log(result);
-            console.log('payment sucess with result.transaction.id'+ result.transaction.id);
+          console.log('payment sucess with result.transaction.id'+ result.transaction.id);
           res.send("Success! Transaction ID: " + result.transaction.id);
         } else {
           console.log(result);
@@ -101,4 +99,99 @@ app.post("/checkout", function (req, res) {
     
   });
 
-  module.exports = app;
+  app.post("/vault", function (req, res) {
+    console.log('vault');
+      console.log(req.body.nonce);
+      gateway.customer.create({
+        firstName: "Reena",
+        lastName: "Kumari",
+        paymentMethodNonce: req.body.nonce
+      }, function (err, result) {
+        if (err) {
+          res.status(500);
+          console.log(err);
+          res.send(err.message);
+        } else if (result.success) {
+          console.log('Customer created with customer id: '+ result.customer.id);
+          console.log('Payment token: '+ result.customer.paymentMethods[0].token);
+          res.send(result);
+        } else {
+          console.log(result);
+          res.status(500);
+          res.send("Error:  " + result.message);
+        }
+      });
+});  
+
+app.post("/vaultwithpayment", function (req, res) {
+  console.log('vault with payment');
+    var nonce = req.body.nonce;
+    console.log(nonce);
+    var saleRequest = {
+       // amount: "1",
+       // merchantAccountId: req.body.currency?req.body.currency:"USD",
+        paymentMethodNonce: nonce,
+       // orderId : req.body.invoice_id,
+        options: {
+         //submitForSettlement: true,
+          storeInVault: true 
+        },
+        // customer : {
+        //   firstName :"Reena",
+        //   lastName : "kumari",
+        //   company : 'PayPal',
+        //   'phone': '123123123',
+        //   'website': 'http://www.example.com',
+        //   'email':'some@testbt.com'
+        // },
+          "deviceData":req.device_data
+      };
+      gateway.transaction.sale(saleRequest, function (err, result) {
+        if (err) {
+          res.status(500);
+          console.log(err);
+          res.send(err.message);
+        } else if (result.success) {
+          console.log(result);
+          console.log('payment sucess with result.transaction.id'+ result.transaction.id);
+          res.send(result);
+        } else {
+          console.log(result);
+          res.status(500);
+          res.send("Error:  " + result.message);
+        }
+      });
+});
+
+app.post("/autopay", function (req, res) {
+  console.log('autopay');
+    console.log(req.body.amount);
+    console.log(req.body.rt_token);
+    console.log(req.body.currency);
+    var saleRequest = {
+        amount: req.body.amount,
+        merchantAccountId: req.body.currency?req.body.currency:"USD",
+        paymentMethodToken: req.body.rt_token,
+        orderId : req.body.invoice_id,
+        options: {
+          submitForSettlement: true
+        }
+      };
+      gateway.transaction.sale(saleRequest, function (err, result) {
+        if (err) {
+          res.status(500);
+          console.log(err);
+          res.send(err.message);
+        } else if (result.success) {
+          console.log(result);
+          console.log('payment sucess with result.transaction.id'+ result.transaction.id);
+          res.send(result);
+        } else {
+          console.log(result);
+          res.status(500);
+          res.send("Error:  " + result.message);
+        }
+      });
+});
+
+module.exports = app;
